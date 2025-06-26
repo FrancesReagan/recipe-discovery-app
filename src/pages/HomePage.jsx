3ecff85ed489a9706cb9cfac5f1f1971ced44233
Frances Reagan James <adtgwen@gmail.com>
2025-06-26 01:19:35 -0400

    home page fixes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -34,11 +34,34 @@ function HomePage() => {
 <p className="text-center text-gray-600 mb-8">
   Browse through different categories and find your next favorite meal.
 </p>
+{/* categories grid */}
+<div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-4">
+{data?.categories?Map((category) => (
 
+<Link
+key={category.idCategory}
+to={`/category/${category.strCategory}`}
+className="block bg-white rounded shadow p-4">
+
+  <img
+  src={category.strCategoryThumb}
+  alt={category.strCategory}
+  className="w-full h-48 object-cover rounded mb-3" />
+  <h2 className="text-xl font-semibold mb-2">
+    {category.strCategory}
+  </h2>
+  <p className="text-gray-600 text-sm">
+    {category.strCategorDescription}
+  </p>
+</Link>
+
+))}
+
+</div>
 
 </div>
 
   );
-}
+};
 
-export default CategoryPage;
+export default HomePage;
