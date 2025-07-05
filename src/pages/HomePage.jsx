9e4cee0ccc9319006f132a86ca4568b5f16f5374
Frances Reagan James <adtgwen@gmail.com>
2025-07-05 16:58:17 -0400

    fixing homepage errors with header and grid

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,39 +1,38 @@
-// Categories grid//
-// routing HomePage//
+// src/pages/HomePage.jsx
 // home page (/) will display all recipe categories//
 
-
-
-
-
 import {Link} from "react-rounter-dom";
-import {useFetch} from "../hooks/useFetch";
-import Spinnger  from "../components/Spinner";
+import useFetch from "../hooks/useFetch";
+import Spinner  from "../components/Spinner";
 import ErrorMessage from "../components/ErrorMessage";
 
 // fetch categories from API here----have error handling here//
 // have categories grid here and key category id here//
 
-function HomePage() => {
-  // fetch all catergories from the API//
-  const { data, loading, error } =
-  useFetch("https://www.themealdb.com/api/json/v1/1/categories.php");
+function HomePage() {
+  // fetch all catergories from the MealDB API//
+  const { data, loading, error } = useFetch(
+     "https://www.themealdb.com/api/json/v1/1/categories.php"
+      );
   // not sure will use the spinner after all--may remove this component//
+  // show spinner animation while loading//
    if (loading) return <Spinner />;
+  //  so error if something goes wrong//
    if (error) return <ErrorMessage message={error}/>;
-}
 
   return (
 
-<div>
-
-
-<h1 className="text-3xl font-bold text-center mb-4">
-  Discover Amazing Recipes
-</h1>
-<p className="text-center text-gray-600 mb-8">
-  Browse through different categories and find your next favorite meal.
-</p>
+<div className="max-w-6xl mx-auto p-4">
+  {/* page header */}
+  <div className="text-center mb-8">
+     <h1 className="text-3xl font-bold text-gray-900 mb-4">
+            Discover Amazing Recipes
+     </h1>
+   <p className="text-gray-600">
+        Browse through different categories and find your next favorite meal.
+    </p>
+  </div>
+  
 {/* categories grid */}
 <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-4">
 {data?.categories?Map((category) => (
