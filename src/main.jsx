c560c3d51623a7073da1e541a94fd860d08547c9
Frances Reagan James <adtgwen@gmail.com>
2025-07-05 19:04:21 -0400

    fixed main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-// entry point with react router//
+// src/main.jsx/.
+// entry point that renders the app//
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import './index.css'
 
+// render the app with react router for navigation//
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <App />
     </BrowserRouter>
   </StrictMode>,
-)
+);
