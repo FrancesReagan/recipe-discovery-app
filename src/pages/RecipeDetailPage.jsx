a753c7b9a9f2c61f5f9d7ca17896c3de8dd6c7f4
Frances Reagan James <adtgwen@gmail.com>
2025-07-05 18:06:55 -0400

    added favorite toggle

--- src/pages/RecipeDetailPage.jsx
+++ src/pages/RecipeDetailPage.jsx
@@ -139,7 +139,27 @@ function RecipeDetailPage() {
       }
     }
 
+    const handleFavoriteToggle = () => {
+      if (favorited) {
+        removeFromFavorites(recipe.idMeal);
+      } else {
+        addToFavorites(recipe);
+      }
+    };
     
+    return (
+      <div className="max-w-4xl mx-auto p-4">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+          <div>
+            <img
+             src={recipe.strMealThumb}
+             alt={recipe.strMeal}
+             className="w-full rounded-lg shadow-lg mb-4"
+             />
+          </div>
+        </div>
+      </div>
+    )
 
 
 
