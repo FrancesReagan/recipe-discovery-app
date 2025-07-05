3d21a1ff0d6b40d6daf879014901abd52cdf7738
Frances Reagan James <adtgwen@gmail.com>
2025-07-05 02:20:55 -0400

    fixed useLocalStorage

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -1,43 +1,39 @@
-// uselocalstorage: syncs state with browser localstorage//
-// browser storage management//
-import { useState, useEffect } from "react";
+// custom hookfor saving data to browser storage//
+import { useState} from "react";
 
 // custom hook for storing data in browser's localStorage//
 export const uselocalstorage = (key, initialValue) => {
-  // state to store the value//
+  // state that syncs with localStorage//
   const [storedValue, setStoredValue] = useState() => {
     try {
-      // try to get the value from localStorage//
-      const item = window.localStorage.getItem(key);
+      // try to get the value from browser storage//
+      const item = localStorage.getItem(key);
 
-    // if it exists, parse it (coverting from string back to original format)//
-    // if  not, use the initial value given//
+    // if data exists, use it (coverting from string back to original format)//
+    // otherwise== use the initial value //
      return item ? JSON.parse(item) : initialValue;
-    } catch (error) {
+    } catch {
     // if something goes wrong, use the initial value//
-    console.error(`Error reading localStorage key "${key}":`, error);
     return initialValue;
     }
-
   });
 
-  // function to update the stored value//
-  const setValue = (value) => {
+  // function to update both state and localStorage//
+  const setStoredValue = (newValue) => {
    try{
-    setStoredValue(value);
+    // update the state//
+    setValue(newValue);
 
-    // save to localStorage(convert to string format)//
-     window.localStorage.setItem(key, JSON.stringify(value));
+    // save to browser storage/localStorage(convert to string format)//
+    localStorage.setItem(key, JSON.stringify(newValue));
   } catch (error) {
-     console.error(`Error setting localStorage key"${key}":`, error);
+     console.error(`Error setting localStorage:`, error);
   }
-
   };
 
   // return the current value and the function to update it//
-  return[storedValue, setValue];
-
-};
+  return[svalue, setStoredValue];
+}
 
 
 
