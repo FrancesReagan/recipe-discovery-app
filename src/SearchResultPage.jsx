8388a709a4a15551877eb6638044942e09cdc877
Frances Reagan James <adtgwen@gmail.com>
2025-07-05 18:43:00 -0400

    added imports and start of SearchResultPage function

--- src/SearchResultPage.jsx
+++ src/SearchResultPage.jsx
@@ -1,7 +1,18 @@
-// search results//
-// search results--search results page//
+// src/pages/SearchResultPage.jsx//
+// page showing search results//
+
+import { useSearchParams } from "react-router-dom";
+import useFetch from "./hooks/useFetch";
+import RecipeCard from "./components/RecipeCard";
+import Spinner from "./components/Spinner";
+import ErrorMessage from "./components/ErrorMessage";
+
 
 function SearchResultPage() {
+// get search query from URL parameters//
+const [searchParams] = useSearchParams();
+const query = searchParams.get("query");
+
 
   return (
 <>
