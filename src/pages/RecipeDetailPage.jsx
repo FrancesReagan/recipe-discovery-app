e8e6d5e9df0d8f64c98ef6816510893657869719
Frances Reagan James <adtgwen@gmail.com>
2025-07-05 17:53:04 -0400

    added ingredient list and measurements

--- src/pages/RecipeDetailPage.jsx
+++ src/pages/RecipeDetailPage.jsx
@@ -57,7 +57,7 @@ function RecipeDetailPage() {
    return (
     <div className="max-w-4xl mx-auto p-4">
       {/* two column layout */}
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+      < className="grid grid-cols-1 lg:grid-cols-2 gap-8">
       {/* left column - image and favorite button */}
       <div>
         <img 
@@ -78,7 +78,7 @@ function RecipeDetailPage() {
        </div>
 
        {/* right column - recipe details */}
-       <div>
+       <>
         
         {/* recipe title */}
         <h1 className="text-3xl font-bold text-gray-900 mb-4">
@@ -98,8 +98,20 @@ function RecipeDetailPage() {
           </span>
         )}
       </div>
+        {/* ingredients list */}
+        <div className="mb-6">
+        <h2 className="text-xl font-semibold mb-3">Ingredients</h2>
+        <ul className="space-y-2">
+          {ingredients.map((item,index) => (
+            <li key={index} className="flex">
+              {/* measurements and ingredients */}
+              <span className="w-20 text-gray-600">{item.measure}</span>
+              <span>{item.ingredient}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
 
-      
        </div>
       </div>
     </div>
